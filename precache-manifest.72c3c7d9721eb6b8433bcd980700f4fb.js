6939514e2c9d4b926cd6e6b69b82a1923ed16633
Tigger Wong <tigger@appit.com.hk>
2021-04-27 21:58:07 +0800

    feat: Add demo

---
+++ precache-manifest.72c3c7d9721eb6b8433bcd980700f4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ce298896ebe0899366c5175023a264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb2b36df0e7cadcd9a37",
+    "url": "/static/css/main.dd458912.chunk.css"
+  },
+  {
+    "revision": "0d6a4d849b93db7500ee",
+    "url": "/static/js/2.47464472.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.47464472.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb2b36df0e7cadcd9a37",
+    "url": "/static/js/main.8490c20b.chunk.js"
+  },
+  {
+    "revision": "2b66ad868130f521470c",
+    "url": "/static/js/runtime-main.0ec482cf.js"
+  }
+]);
